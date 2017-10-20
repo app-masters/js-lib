fa3d60c92f09ef4a27134e76ed65980d9c3b3ff5
Matheus Rezende <matheus.appmasters@gmail.com>
2017-10-20 16:15:27 -0200

    gitignore

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, 'src/') + '/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+};
