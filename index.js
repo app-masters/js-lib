c7cc0d9b70ac526b3caa92e8a33be6360b180871
Matheus Rezende <matheus.appmasters@gmail.com>
2017-10-23 16:30:02 -0200

    dont know how to describe

--- index.js
+++
@@ -1,9 +0,0 @@
-import Http from './src/http';
-import Rollbar from './src/rollbar';
-import JSError from './src/jsError';
-import DateTime from './src/dateTime';
-import VersionCheck from './src/version';
-import Text from './src/text';
-import AMStorage from './src/storage';
-
-export {Http, Rollbar, JSError, DateTime, Text, VersionCheck, AMStorage};
