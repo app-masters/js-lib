b0bfbf8b161c0da8a7d7fb03644c50a9e2ea8b3b
Matheus Rezende <matheus.appmasters@gmail.com>
2017-10-23 16:04:31 -0200

    jslib not compiled anymore

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Http from './http';
-import Rollbar from './rollbar';
-import JSError from './jsError';
-import DateTime from './dateTime';
-import VersionCheck from './version';
-import Text from './text';
-import AMStorage from './storage';
-
-export {Http, Rollbar, JSError, DateTime, Text, VersionCheck, AMStorage};
