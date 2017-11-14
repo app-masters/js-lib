02f3423f87dfee590b69f3e3352fe14f97f40950
João Baraky <baraky.appmasters@gmail.com>
2017-11-14 09:38:41 -0200

    Fixes in export

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,11 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-## [1.0.9] - 2017-11-9
+## [1.0.9] / [1.0.10] - 2017-11-14
 ### Added
 - Object Handler for javascript objects methods
+### Changed
+- Fixes in export
 
 ## [1.0.8] - 2017-11-9
 ### Changed
--- src/objHandler.js
+++ src/objHandler.js
@@ -2,7 +2,7 @@ import Text from './text';
 
 class ObjHandler {
 
-    // get child or child of child...
+    // get child or child of child or child of child of child...
     // Key can be "key.otherKey"
     static getChild(obj, key) {
         let arr = key.split(".");
@@ -23,4 +23,29 @@ class ObjHandler {
             }
         });
     };
+
+    // Remove objects with a duplicate key from array of objects
+    // (will keep the last found)
+    static removeDuplicates = (objArray, key) => {
+        let newArray = [];
+        for (let index in objArray) {
+            let foundIndex = null;
+            const found = newArray.find((relation, i) => {
+                if (ObjHandler.getChild(relation,key) === ObjHandler.getChild(objArray[index],key)) {
+                    foundIndex = i;
+                    return true;
+                }
+                return false;
+            });
+            if (found === undefined) {
+                newArray.push(objArray[index]);
+            } else {
+                newArray[foundIndex] = found;
+            }
+        }
+        return newArray;
+    };
+
 }
+
+export default ObjHandler;
