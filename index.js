bae0111df37424423febeea523e7ff2a18e7f8ab
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2018-02-26 11:54:07 -0300

    Notification added

---
+++ index.js
@@ -0,0 +1,25 @@
+import Http from './dist/http';
+import Rollbar from './dist/rollbar';
+import JSError from './dist/jsError';
+import DateTime from './dist/dateTime';
+import VersionCheck from './dist/version';
+import Text from './dist/text';
+import AMStorage from './dist/storage';
+import AppBootstrap from './dist/appBootstrap';
+import ObjHandler from './dist/objHandler';
+import HttpErrorHandler from './dist/httpErrorHandler';
+import Notification from './dist/notification';
+
+export {
+    Http,
+    Rollbar,
+    JSError,
+    DateTime,
+    Text,
+    VersionCheck,
+    AMStorage,
+    AppBootstrap,
+    ObjHandler,
+    HttpErrorHandler,
+    Notification
+};
