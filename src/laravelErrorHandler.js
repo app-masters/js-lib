f1e923edf444a25094ef9bd6763f8a80cf76e1fe
João Fernando Baraky <jfbaraky@gmail.com>
2019-01-03 11:59:45 -0200

    Setting 'user' error level

--- src/laravelErrorHandler.js
+++ src/laravelErrorHandler.js
@@ -4,7 +4,7 @@ class LaravelErrorHandler {
     static handle (error) {
         let errorObj =  LaravelErrorHandler.errorLibrary(error);
 
-        if (errorObj.level){
+        if (errorObj.level !== 'user'){
             Rollbar[errorObj.level](errorObj);
         }
 
@@ -13,7 +13,7 @@ class LaravelErrorHandler {
 
     static errorLibrary(errorObj){
         if (errorObj.status === 405 || errorObj.status >= 500){
-            // Server error, don't show
+            // Server error, don't show original
             errorObj.body = {error: ['Falha no servidor']};
             errorObj.level = 'critical';
         } else if (errorObj.status >= 400 && errorObj.status <= 430) {
@@ -23,6 +23,7 @@ class LaravelErrorHandler {
                 body.error = body.error.map(error => error.replace(/ *\[[^)]*\] */g, ''));
             }
             errorObj.body = body;
+            errorObj.level = 'user'; // User error, don't send to Rollbar
         } else {
             // Another error (maybe a browser error)
             errorObj.body = {error: [errorObj.remote]};
